e1754a372a4bc845e32f5bfa6713992b91d752c8
polsevev <rolf.martin@glomsrud.no>
2021-04-06 21:56:03 +0200

    lol

--- bot.py
+++ bot.py
@@ -3,6 +3,7 @@ import os
 from discord.ext import commands
 
 
+
 client = discord.Client()
 
 GUILD = os.getenv('Rolf\'s bot testing')
@@ -11,7 +12,10 @@ bot = commands.Bot(command_prefix='$')
 
 
 roleChannelID = 824001207436836875
-emojis = ["😀", "😃"]
+channelGuild = 823983756993888277
+emojis = ["\U0001F49C", "\U0001F49A", "\U0001F5A4","\U0001FAD0","\U0001F499", "\U0001F338", "\U0001F90D", "\U0001F7EA", "\U0001F7E9", "\U0001F49B"]
+roleNames = ["Purple", "Green", "Black", "Blue", "Cyan", "Pink", "White", "Dark Purple", "Light Green", "Yellow"]
+roleId = []
 
 changeRoleMessageBrief = "Change the message of the setRole channel"
 changeRoleMessageFull = "Change the message of setRole by typing the new message inside \" \" after the command"
@@ -22,58 +26,90 @@ pingFull = "Ping the bot and it will respond with pong if online"
 announceBrief = "Will create an anouncement in the anouncement channel"
 announceFull = "Will make an announcement with them essage inside \"MESSAGE HERE\" after the command"
 
+standardRoleMessage = ""
 
 
 
 @bot.command(brief = changeRoleMessageBrief, description=changeRoleMessageFull)
-async def changeRoleMessage(ctx, arg):
-    channel = bot.get_channel(roleChannelID)
-    await channel.purge(limit=int(100))
-    message = await channel.send(arg)
+@commands.has_permissions(administrator=True)
+async def resetRoleMessage(ctx):
+    roleChannel = bot.get_channel(roleChannelID)
+    messages = await roleChannel.history(limit=10).flatten()
+    for message in messages:
+        await message.delete()
+    message = await roleChannel.send(standardRoleMessage)
     for emoji in emojis:
         await message.add_reaction(emoji)
 
 
-@bot.command()
+
+@bot.command(pass_context=True)
+@commands.has_permissions(administrator=True)
 async def say(ctx, arg):
     await ctx.send(arg)
 
 
 @bot.command()
+@commands.has_permissions(administrator=True)
 async def ping(ctx):
     print(ctx.message.author)
     await ctx.send("pong")
+
+@bot.command()
+@commands.has_permissions(administrator=True)
+async def addRoleMessage(ctx, arg):
+    roleChannel = bot.get_channel(roleChannelID)
+    message = await roleChannel.send(arg)
+    for emoji in emojis:
+        await message.add_reaction(emoji)
 
 @bot.event
 async def on_ready():
     roleChannel = bot.get_channel(roleChannelID)
     messages = await roleChannel.history(limit=10).flatten()
+
+    for x in roleNames:
+        role = discord.utils.find(lambda r : r.name == x, bot.get_guild(channelGuild).roles)
+        roleId.append(role.id)
+
+    global standardRoleMessage
+    standardRoleMessage = (
+            f"To choose color, please react with the following emoji for the specified color: \n"
+            f":purple_heart:  = <@&{roleId[0]}>\n"
+            f":green_heart:  = <@&{roleId[1]}>\n"
+            f":black_heart:  = <@&{roleId[2]}>\n"
+            f":blueberries:  = <@&{roleId[3]}>\n"
+            f":blue_heart:  = <@&{roleId[4]}>\n"
+            f":cherry_blossom:  = <@&{roleId[5]}>\n"
+            f":white_heart:  = <@&{roleId[6]}>\n"
+            f":purple_square:  = <@&{roleId[7]}>\n"
+            f":green_square:  = <@&{roleId[8]}>\n"
+            f":yellow_heart:  = <@&{roleId[9]}>\n"
+        )
     if (len(messages) < 1):
-        message = await roleChannel.send("React with 😀 To be added to Role1, React with 😃 to be added to Role2")
+        message = await roleChannel.send(standardRoleMessage)
         for emoji in emojis:
             await message.add_reaction(emoji)
     print("Bot is ready")
 
-
-
 @bot.command(brief=announceBrief, description=announceFull)
+@commands.has_permissions(administrator=True)
 async def announce(ctx, arg1):
     anounceChannel = bot.get_channel(823988506158170125)
     await anounceChannel.send(arg1)
+
+
 
 @bot.event
 async def on_raw_reaction_add(payload):
     if payload.channel_id == roleChannelID:
         guild = bot.get_guild(payload.guild_id)
         member = payload.member
-        if str(payload.emoji) == "😀":
-            role = discord.utils.find(lambda r : r.name == "Role1", guild.roles)
-            await member.add_roles(role)
-        elif str(payload.emoji) == "😃":
-            role = discord.utils.find(lambda r: r.name == "Role2", guild.roles)
-            await member.add_roles(role)
-        else:
-            print("Smack")
+        for x in range(0, len(emojis)):
+            if(str(payload.emoji)) == emojis[x]:
+                print("It was true")
+                role = guild.get_role(roleId[x])
+                await member.add_roles(role)
     print(payload.user_id)
     print(str(payload.emoji))
 
@@ -82,22 +118,12 @@ async def on_raw_reaction_remove(payload):
     if(payload.channel_id == roleChannelID):
         guild = bot.get_guild(payload.guild_id)
         member = await guild.fetch_member(payload.user_id)
-        if str(payload.emoji) == "😀":
-            role = discord.utils.find(lambda r : r.name == "Role1", guild.roles)
-            if(role == "error"):
-                print("Couldn't find role")
-            else:
+        for x in range(0, len(emojis)):
+            if(str(payload.emoji)) == emojis[x]:
+                print("It was true")
+                role = role = guild.get_role(roleId[x])
                 await member.remove_roles(role)
-        elif str(payload.emoji) == "😃":
-            role = discord.utils.find(lambda r: r.name == "Role2", guild.roles)
-            if (role == "error"):
-                print("Couldn't find role")
-            else:
-                await member.remove_roles(role)
-        else:
-            print("Smack")
-    print(payload.user_id)
-    print(str(payload.emoji))
+                print("role removed")
 
 
 
